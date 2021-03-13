5af7a5b8113de6198cb3b517f5517041fe8bcb64
Xavier Rotllan-Puig <xavi.rotllan.puig@gmail.com>
2021-03-13 11:04:03 +0100

    threshold for potential local productivity

--- R/11_CombinedAssessment.R
+++ R/11_CombinedAssessment.R
@@ -6,40 +6,47 @@
 #' @title LPD_CombAssess
 #' @description LPD_CombAssess combines a 'LandProd_change' map (RasterLayer) with a 'LandProd_current'
 #' map (RasterLayer), giving a 5-classes map ranging from declining to increasing land
-#' productivity. 'LandProd_current' is reclassified into two classes: pixels with less than 50% of the
-#' highest local production (within the EFT) and pixels with more or equal to their 50%.
+#' productivity. 'LandProd_current' is reclassified into two classes: pixels with less than 'local_prod_threshold'
+#' (in percentage; 50 by default) of potential local productivity (within the EFT) and pixels with more or equal to
+#' 'local_prod_threshold'.
+#'
 #' If 'LandProd_current' = NULL, 'LandProd_change' is directly reclassified into the same 5-classes map
 #' without using 'LandProd_current'. See the ATBD for the way pixels are reclassified.
 #'
+#'
 #' @details
-#' LandProd_change c(1:6, 8:9)       &  LandProd_current  < 50  <- 1 Declining land productivity
+#' LandProd_change c(1:6, 8:9)       &  LandProd_current  < 'local_prod_threshold'  <- 1 Declining land productivity
 #'
-#' LandProd_change c(3, 6)           &  LandProd_current >= 50  <- 1 Declining land productivity
+#' LandProd_change c(3, 6)           &  LandProd_current >= 'local_prod_threshold'  <- 1 Declining land productivity
 #'
-#' LandProd_change c(7)              &  LandProd_current  < 50  <- 2 Early signs of decline of land productivity
+#' LandProd_change c(7)              &  LandProd_current  < 'local_prod_threshold'  <- 2 Early signs of decline of land productivity
 #'
-#' LandProd_change c(1:2, 4:5, 8:9)  &  LandProd_current >= 50  <- 2 Early signs of decline of land productivity
+#' LandProd_change c(1:2, 4:5, 8:9)  &  LandProd_current >= 'local_prod_threshold'  <- 2 Early signs of decline of land productivity
 #'
-#' LandProd_change c(7)              &  LandProd_current >= 50  <- 3 Negative fluctuation (stable, but stressed land prod.)
+#' LandProd_change c(7)              &  LandProd_current >= 'local_prod_threshold'  <- 3 Negative fluctuation (stable, but stressed land prod.)
 #'
 #' LandProd_change c(10:12)                                     <- 3 Negative fluctuation (stable, but stressed land prod.)
 #
 #' LandProd_change c(13:15)                                     <- 4 Positive fluctuation (stable, not stressed land prod.)
 #'
-#' LandProd_change c(16:17, 19)      &  LandProd_current  < 50  <- 4 Positive fluctuation (stable, not stressed land prod.)
+#' LandProd_change c(16:17, 19)      &  LandProd_current  < 'local_prod_threshold'  <- 4 Positive fluctuation (stable, not stressed land prod.)
 #'
-#' LandProd_change c(18, 20:22)      &  LandProd_current  < 50  <- 5 Increasing land productivity
+#' LandProd_change c(18, 20:22)      &  LandProd_current  < 'local_prod_threshold'  <- 5 Increasing land productivity
 #'
-#' LandProd_change c(16:22)          &  LandProd_current >= 50  <- 5 Increasing land productivity
+#' LandProd_change c(16:22)          &  LandProd_current >= 'local_prod_threshold'  <- 5 Increasing land productivity
 #'
 #'
 #' Values = 0 in the final map indicates that there is a scarcity of data in the productivity variable
 #' (i.e. only 1 year with data), so that the indicator cannot be calculated
 #'
+#' 'local_prod_threshold' is the threshold used to classify 'LandProd_current' into pixels with low or high
+#' potential productivity within its Ecosystem Functional Type
+#'
 #'
 #' @import raster
 #' @param LandProd_change RasterLayer object (or its file name). Land Productivity Long Term Change Map
 #' @param LandProd_current RasterLayer object (or its file name). Land Productivity Current Status Map
+#' @param local_prod_threshold Numeric. Potential local productivity threshold (within the Ecosystem Functional Type) in percentage. Optional. Default = 50
 #' @param filename Character. Output filename. Optional
 #' @return RasterLayer
 #' @name LPD_CombAssess
@@ -74,6 +81,7 @@
 
 
 LPD_CombAssess <- function(LandProd_change = NULL, LandProd_current = NULL,
+                           local_prod_threshold = 50,
                            filename = ""){
 
   ## Reading in rasters ####
@@ -92,24 +100,26 @@ LPD_CombAssess <- function(LandProd_change = NULL, LandProd_current = NULL,
       stop("Please provide objects of classe RasterLayer (or file names to read in some)")
     }
 
+    if(local_prod_threshold < 1 | local_prod_threshold > 100) stop("'local_prod_threshold' has to be in %")
+
     ## Checking for same extent/resolution
     if(any(extent(LandProd_change) != extent(LandProd_current), res(LandProd_change) != res(LandProd_current)))
       stop("LandProd_change and LandProd_current must have same extent and resolution")
 
     ## Combined Assessment ####
     LPD_CombAssess <- LandProd_change
-    LPD_CombAssess[LandProd_change %in% c(1:6, 8:9)      & LandProd_current  < 50] <- 1      # 1(d): Declining land productivity
-    LPD_CombAssess[LandProd_change %in% c(3, 6)          & LandProd_current >= 50] <- 1      # 1(d): Declining land productivity
-    LPD_CombAssess[LandProd_change %in% c(7)             & LandProd_current  < 50] <- 2      # 2(ew): Early signs of decline of land productivity
-    LPD_CombAssess[LandProd_change %in% c(1:2, 4:5, 8:9) & LandProd_current >= 50] <- 2      # 2(ew): Early signs of decline of land productivity
-    LPD_CombAssess[LandProd_change %in% c(7)             & LandProd_current >= 50] <- 3      # 3(nf): Negative fluctuation (stable, but stressed land prod.)
-    LPD_CombAssess[LandProd_change %in% c(10:12)                                 ] <- 3      # 3(nf): Negative fluctuation (stable, but stressed land prod.)
-    LPD_CombAssess[LandProd_change %in% c(13:15)                                 ] <- 4      # 4(pf): Positive fluctuation (stable, not stressed land prod.)
-    LPD_CombAssess[LandProd_change %in% c(16:17, 19)     & LandProd_current  < 50] <- 4      # 4(pf): Positive fluctuation (stable, not stressed land prod.)
-    LPD_CombAssess[LandProd_change %in% c(18, 20:22)     & LandProd_current  < 50] <- 5      # 5(i): Increasing land productivity
-    LPD_CombAssess[LandProd_change %in% c(16:22)         & LandProd_current >= 50] <- 5      # 5(i): Increasing land productivity
-    #LPD_CombAssess[LandProd_change %in% c(16:17, 19:20)  & LandProd_current >= 50] <- 5      # 5(i): Increasing land productivity
-    #LPD_CombAssess[LandProd_change %in% c(18, 21:22)     & LandProd_current >= 50] <- 6      # 6(si): Strongly increasing land productivity
+    LPD_CombAssess[LandProd_change %in% c(1:6, 8:9)      & LandProd_current  < local_prod_threshold] <- 1      # 1(d): Declining land productivity
+    LPD_CombAssess[LandProd_change %in% c(3, 6)          & LandProd_current >= local_prod_threshold] <- 1      # 1(d): Declining land productivity
+    LPD_CombAssess[LandProd_change %in% c(7)             & LandProd_current  < local_prod_threshold] <- 2      # 2(ew): Early signs of decline of land productivity
+    LPD_CombAssess[LandProd_change %in% c(1:2, 4:5, 8:9) & LandProd_current >= local_prod_threshold] <- 2      # 2(ew): Early signs of decline of land productivity
+    LPD_CombAssess[LandProd_change %in% c(7)             & LandProd_current >= local_prod_threshold] <- 3      # 3(nf): Negative fluctuation (stable, but stressed land prod.)
+    LPD_CombAssess[LandProd_change %in% c(10:12)                                                   ] <- 3      # 3(nf): Negative fluctuation (stable, but stressed land prod.)
+    LPD_CombAssess[LandProd_change %in% c(13:15)                                                   ] <- 4      # 4(pf): Positive fluctuation (stable, not stressed land prod.)
+    LPD_CombAssess[LandProd_change %in% c(16:17, 19)     & LandProd_current  < local_prod_threshold] <- 4      # 4(pf): Positive fluctuation (stable, not stressed land prod.)
+    LPD_CombAssess[LandProd_change %in% c(18, 20:22)     & LandProd_current  < local_prod_threshold] <- 5      # 5(i): Increasing land productivity
+    LPD_CombAssess[LandProd_change %in% c(16:22)         & LandProd_current >= local_prod_threshold] <- 5      # 5(i): Increasing land productivity
+    #LPD_CombAssess[LandProd_change %in% c(16:17, 19:20)  & LandProd_current >= local_prod_threshold] <- 5      # 5(i): Increasing land productivity
+    #LPD_CombAssess[LandProd_change %in% c(18, 21:22)     & LandProd_current >= local_prod_threshold] <- 6      # 6(si): Strongly increasing land productivity
     LPD_CombAssess[is.na(LandProd_change)] <- NA
 
 
